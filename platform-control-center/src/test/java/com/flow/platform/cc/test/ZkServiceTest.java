4e4bcbed2568372ce8c8c9f387f029ce0b00abcf
Yang Guo <gy@fir.im>
2017-05-18 01:49:24 +0800

    ut for send command to agent

--- platform-control-center/src/test/java/com/flow/platform/cc/test/ZkServiceTest.java
+++ platform-control-center/src/test/java/com/flow/platform/cc/test/ZkServiceTest.java
@@ -1,7 +1,9 @@
 package com.flow.platform.cc.test;
 
 import com.flow.platform.cc.service.ZkService;
+import com.flow.platform.util.zk.ZkCmd;
 import com.flow.platform.util.zk.ZkNodeHelper;
+import com.google.gson.Gson;
 import org.apache.zookeeper.ZooKeeper;
 import org.junit.Assert;
 import org.junit.Before;
@@ -63,5 +65,38 @@ public class ZkServiceTest extends TestBase {
         Assert.assertEquals(2, zkService.onlineAgent(zoneName).size());
         Assert.assertTrue(zkService.onlineAgent(zoneName).contains("test-agent-001"));
         Assert.assertTrue(zkService.onlineAgent(zoneName).contains("test-agent-001-busy"));
+    }
+
+    @Test
+    public void should_send_cmd_to_agent() throws InterruptedException {
+        // given:
+        String zoneName = zkZone.split(";")[0];
+        String agentName = "test-agent-001";
+        Assert.assertEquals(0, zkService.onlineAgent(zoneName).size());
+
+        String agentPath = String.format("/flow-agents/%s/%s", zoneName, agentName);
+        ZkNodeHelper.createEphemeralNode(zkClient, agentPath, "");
+        Thread.sleep(1000);
+
+        // when: send command
+        ZkCmd cmd = new ZkCmd(ZkCmd.Type.RUN_SHELL, "/test.sh");
+        zkService.sendCommand(zoneName, agentName, cmd);
+
+        // then:
+        byte[] raw = ZkNodeHelper.getNodeData(zkClient, agentPath, null);
+        Gson gson = new Gson();
+        ZkCmd loaded = gson.fromJson(new String(raw), ZkCmd.class);
+
+        Assert.assertEquals(cmd, loaded);
+    }
+
+    @Test
+    public void should_raise_exception_agent_not_exit() {
+
+    }
+
+    @Test
+    public void should_raise_exception_agent_busy() {
+
     }
 }
