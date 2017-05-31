0c196b25c814fefc35ae09993ce4b9b5221a1753
Yang Guo <gy@fir.im>
2017-05-31 18:41:10 +0800

    upload zipped cmd log in report manager

--- platform-agent/src/main/java/com/flow/platform/agent/ReportManager.java
+++ platform-agent/src/main/java/com/flow/platform/agent/ReportManager.java
@@ -3,11 +3,16 @@ package com.flow.platform.agent;
 import com.flow.platform.domain.Cmd;
 import com.flow.platform.domain.CmdReport;
 import com.flow.platform.domain.CmdResult;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
@@ -61,8 +66,8 @@ public class ReportManager {
      * @return
      */
     public boolean cmdReportSync(final String cmdId,
-                                       final Cmd.Status status,
-                                       final CmdResult result) {
+                                 final Cmd.Status status,
+                                 final CmdResult result) {
         try {
             cmdReportSync(cmdId, status, result, 5);
             return true;
@@ -88,26 +93,44 @@ public class ReportManager {
         StringEntity entity = new StringEntity(postCmd.toJson(), ContentType.APPLICATION_JSON);
         post.setEntity(entity);
 
-        CloseableHttpClient client = HttpClients.createDefault();
-        try {
-            HttpResponse response = client.execute(post);
-            int code = response.getStatusLine().getStatusCode();
-            if (code != HttpStatus.SC_OK) {
-                throw new RuntimeException("Fail to report cmd status to : " + url);
-            }
-            Logger.info(String.format("Cmd %s report status %s", cmdId, status));
-        } catch (Throwable e) {
-            if (retry > 0) {
-                cmdReportSync(cmdId, status, result, retry - 1);
-                return;
-            }
-            throw new RuntimeException(e);
-        } finally {
-            client.close();
-        }
+        String successMsg = String.format("Fail to report cmd status to : %s", url);
+        String failMsg = String.format("Cmd %s report status %s", cmdId, status);
+        httpSend(post, retry, successMsg, failMsg);
     }
 
-    private void cmdLogUploadSync(final String cmdId, final Path logPath) {
+    private void cmdLogUploadSync(final String cmdId, final Path logPath, final int retry) throws IOException {
+        FileBody zippedFile = new FileBody(logPath.toFile(), ContentType.create("application/zip"));
+        HttpEntity entity = MultipartEntityBuilder.create()
+                .addPart("file", zippedFile)
+                .addPart("cmdId", new StringBody(cmdId, ContentType.TEXT_PLAIN))
+                .build();
 
+        String url = Config.agentConfig().getCmdLogUrl();
+        HttpPost post = new HttpPost(url);
+        post.setEntity(entity);
+
+        String successMsg = String.format("Fail to upload zipped cmd log to : %s ", url);
+        String failMsg = String.format("Zipped cmd log uploaded %s", logPath);
+        httpSend(post, retry, successMsg, failMsg);
+    }
+
+    private static HttpResponse httpSend(final HttpUriRequest request,
+                                         final int retry,
+                                         final String successMsg,
+                                         final String failMsg) {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpResponse response = client.execute(request);
+            int code = response.getStatusLine().getStatusCode();
+            if (code != HttpStatus.SC_OK) {
+                throw new RuntimeException(successMsg);
+            }
+            Logger.info(failMsg);
+            return response;
+        } catch (Throwable e) {
+            if (retry > 0) {
+                return httpSend(request, retry - 1, successMsg, failMsg);
+            }
+            throw new RuntimeException(e);
+        }
     }
 }
