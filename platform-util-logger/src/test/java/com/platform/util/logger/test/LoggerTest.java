94a33dba6b3726b5dd51bf60f5e4d2ee518f0ad3
Yang Guo <gy@fir.im>
2017-06-10 14:54:24 +0800

    remove duplicate java version settings

---
+++ platform-util-logger/src/test/java/com/platform/util/logger/test/LoggerTest.java
@@ -0,0 +1,15 @@
+package com.platform.util.logger.test;
+
+import org.junit.Test;
+
+/**
+ * Created by gy@fir.im on 10/06/2017.
+ * Copyright fir.im
+ */
+public class LoggerTest {
+
+    @Test
+    public void should_print_log() {
+
+    }
+}
