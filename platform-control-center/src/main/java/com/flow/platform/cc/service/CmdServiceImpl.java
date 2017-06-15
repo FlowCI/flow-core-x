68ebdca9e6e53241036dd3dec30f178e767e902a
Yang Guo <gy@fir.im>
2017-06-15 13:52:59 +0800

    add log for timeout cmd

--- platform-control-center/src/main/java/com/flow/platform/cc/service/CmdServiceImpl.java
+++ platform-control-center/src/main/java/com/flow/platform/cc/service/CmdServiceImpl.java
@@ -269,6 +269,7 @@ public class CmdServiceImpl extends ZkServiceBase implements CmdService {
             if (cmd.getType() == CmdType.RUN_SHELL && cmd.isCurrent()) {
                 if (isTimeout(cmd)) {
                     // kill current running cmd and report status
+                    LOGGER.traceMarker("checkTimeoutTask", "Cmd %s timeout", cmd);
                     send(new CmdBase(cmd.getAgentPath(), CmdType.KILL, null));
                     report(cmd.getId(), CmdStatus.TIMEOUT_KILL, cmd.getResult());
                 }
