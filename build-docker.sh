9370049762ec35ee743ba829fef511a0e7ffb360
Yang Guo <gy@fir.im>
2017-08-06 16:23:31 +0800

    add docker file for agent

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-# build artifact of each components
+# The following docker will be generated:
+#  - flow.ci.backend: it includes api and control center
+#  - flow.ci.agent:
+
+# How to run flow.ci
+# - backend: 'docker-compose up'
+# - agent: docker run --network=host -e FLOW_ZOOKEEPER_HOST=127.0.0.1:2181 -e FLOW_AGENT_ZONE=default flow.ci.agent
+
+# mvn build artifact of each components
 mvn clean install -DskipTests=true
 
-# build docker image via compose
+# build docker image for flow.ci backend
+docker build -t flow.ci.backend -f ./Dockerfile-backend .
+
+# build docker compose service for flow.ci backend
 docker-compose rm -f
 docker-compose build
+
+# build docker image for flow.ci agent
+docker build -t flow.ci.agent -f ./Dockerfile-agent .
