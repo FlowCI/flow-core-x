ed3b85ad5233b20afc052f7e85c84dc8ed1fffe7
stargwq <liuhailianggwq@gmail.com>
2017-10-17 16:45:44 +0800

    send register email

--- platform-api/src/main/java/com/flow/platform/api/config/AppConfig.java
+++ platform-api/src/main/java/com/flow/platform/api/config/AppConfig.java
@@ -32,6 +32,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
@@ -136,6 +137,11 @@ public class AppConfig extends AppConfigBase {
         VelocityEngine ve = new VelocityEngine();
         ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
         ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+
+        ve.setProperty(Velocity.ENCODING_DEFAULT, "UTF-8");
+        ve.setProperty(Velocity.INPUT_ENCODING, "UTF-8");
+        ve.setProperty(Velocity.OUTPUT_ENCODING, "UTF-8");
+
         ve.init();
         return ve;
     }
