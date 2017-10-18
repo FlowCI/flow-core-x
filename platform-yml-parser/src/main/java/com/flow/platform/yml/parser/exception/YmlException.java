0cc8a785cdc49cfc452debfc21b2e6ffc28a6c2f
will <yh@fir.im>
2017-10-18 16:39:44 +0800

    change format to +

--- platform-yml-parser/src/main/java/com/flow/platform/yml/parser/exception/YmlException.java
+++ platform-yml-parser/src/main/java/com/flow/platform/yml/parser/exception/YmlException.java
@@ -22,10 +22,10 @@ package com.flow.platform.yml.parser.exception;
 public class YmlException extends RuntimeException {
 
     public YmlException(String message) {
-        super(String.format("Illegal yml format: %s", message));
+        super("Illegal yml format: " + message);
     }
 
     public YmlException(String message, Throwable cause) {
-        super(String.format("Illegal yml format: %s", message), cause);
+        super("Illegal yml format: " + message, cause);
     }
 }
