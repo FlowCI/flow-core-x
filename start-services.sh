b26b3014ea78f1d0c4676dbfc6d193c255778f60
Yang Guo <gy@fir.im>
2017-10-25 09:19:49 +0800

    add name for related docker image

--- start-services.sh
+++ start-services.sh
@@ -8,5 +8,5 @@
 # pull rabbitmq 3.6.10 docker image
 # docker pull rabbitmq:3.6
 
-docker run -d -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 mysql:5.6
-docker run -d --hostname flow-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3.6-management
+docker run -d --name mysql-5.6 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 mysql:5.6
+docker run -d --name rabbitmq-3.6 --hostname flow-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3.6-management
