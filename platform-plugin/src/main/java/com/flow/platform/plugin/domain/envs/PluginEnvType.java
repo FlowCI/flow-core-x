740155b78f20532468fc4ddef83146bf502251ed
will <yh@fir.im>
2017-11-30 10:20:22 +0800

    add pluginEnvType Handler

--- platform-plugin/src/main/java/com/flow/platform/plugin/domain/envs/PluginEnvType.java
+++ platform-plugin/src/main/java/com/flow/platform/plugin/domain/envs/PluginEnvType.java
@@ -16,26 +16,125 @@
 
 package com.flow.platform.plugin.domain.envs;
 
+import com.google.common.base.Strings;
+
 /**
  * @author yh@firim
  */
 public enum PluginEnvType {
 
-    STRING("string"),
+    STRING("string", new StringHandler()),
 
-    INTEGER("integer"),
+    INTEGER("integer", new IntegerHandler()),
 
-    BOOLEAN("boolean"),
+    BOOLEAN("boolean", new BooleanHandler()),
 
-    LIST("list");
+    LIST("list", new ListHandler());
+
 
     private String value;
 
-    PluginEnvType(String value) {
+    private Handler handler;
+
+    PluginEnvType(String value, Handler handler) {
         this.value = value;
+        this.handler = handler;
     }
 
     public String getValue() {
         return value;
     }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    private interface Handler<T> {
+
+        Boolean isValidated(String value);
+
+        T converter(String value);
+    }
+
+    private static class StringHandler implements Handler<String> {
+
+        @Override
+        public Boolean isValidated(String value) {
+            return true;
+        }
+
+        @Override
+        public String converter(String value) {
+            return value;
+        }
+    }
+
+    private static class IntegerHandler implements Handler<Integer> {
+
+        @Override
+        public Boolean isValidated(String value) {
+            try {
+                integerOfString(value);
+            } catch (Throwable throwable) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public Integer converter(String value) {
+            return integerOfString(value);
+        }
+    }
+
+    private static class BooleanHandler implements Handler<Boolean> {
+
+        @Override
+        public Boolean isValidated(String value) {
+            try {
+                booleanOfString(value);
+            } catch (Throwable throwable) {
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        public Boolean converter(String value) {
+            return booleanOfString(value);
+        }
+    }
+
+    private static class ListHandler implements Handler<String> {
+
+        @Override
+        public Boolean isValidated(String value) {
+            return true;
+        }
+
+        @Override
+        public String converter(String value) {
+            return value;
+        }
+    }
+
+    private static boolean booleanOfString(String value) {
+        if (Strings.isNullOrEmpty(value)) {
+            return false;
+        } else {
+            return Boolean.valueOf(value);
+        }
+    }
+
+
+    private static Integer integerOfString(String value) {
+        if (Strings.isNullOrEmpty(value)) {
+            return null;
+        } else {
+            return Integer.valueOf(value);
+        }
+    }
+
 }
