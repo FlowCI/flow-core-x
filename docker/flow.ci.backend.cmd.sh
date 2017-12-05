eaf25c6d21fdf144a515604bffdd7e2bff1253b3
will <yh@fir.im>
2017-12-05 08:23:45 +0800

    add install mysql version

--- docker/flow.ci.backend.cmd.sh
+++ docker/flow.ci.backend.cmd.sh
@@ -3,6 +3,15 @@ set -e
 
 cmd="$@"
 
+read -r -d '' rootCreate <<-EOSQL
+  SET @@SESSION.SQL_LOG_BIN=0;
+  DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost') ;
+  SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_PASSWORD}') ;
+  GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
+  DROP DATABASE IF EXISTS test ;
+  FLUSH PRIVILEGES ;
+EOSQL
+
 # Start Mysql
 service mysql start
 
@@ -11,7 +20,15 @@ if [[ ${#MYSQL_HOST} -eq 0 ]];then
     exit;
 fi
 
-mysql --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD -e "use mysql;UPDATE user SET plugin='mysql_native_password' WHERE User='root';FLUSH PRIVILEGES;"
+mysql --host=$MYSQL_HOST --user=$MYSQL_USER -e 'select version();' &> /dev/null;
+isMysqlInit=$?
+if [[ ${isMysqlInit} -eq 0 ]];then
+  # mysql is init
+  mysql --host=$MYSQL_HOST --user=$MYSQL_USER -e "use mysql;UPDATE user SET plugin='mysql_native_password' WHERE User='root';FLUSH PRIVILEGES;"
+  mysql --host=$MYSQL_HOST --user=$MYSQL_USER -e "${rootCreate}"
+
+  service mysql restart
+fi
 
 # First: waiting mysql up to do next cmd
 # start monitor mysql start up or not every one second
