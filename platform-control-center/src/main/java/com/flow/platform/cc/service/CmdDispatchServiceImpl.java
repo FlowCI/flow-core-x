22e0361367a260acf9cff94663fe4efc2d41bec3
will <yh@fir.im>
2017-12-08 10:22:20 +0800

    check cmd type and status while checking cmd timeout

--- platform-control-center/src/main/java/com/flow/platform/cc/service/CmdDispatchServiceImpl.java
+++ platform-control-center/src/main/java/com/flow/platform/cc/service/CmdDispatchServiceImpl.java
@@ -158,14 +158,22 @@ public class CmdDispatchServiceImpl extends ApplicationEventService implements C
         List<Cmd> workingCmdList = cmdService.listWorkingCmd(null);
 
         for (Cmd cmd : workingCmdList) {
-            if (DateUtil.isTimeOut(cmd.getCreatedDate(), ZonedDateTime.now(), cmd.getTimeout())) {
-                Cmd killCmd = cmdService.create(new CmdInfo(cmd.getAgentPath(), CmdType.KILL, null));
-                dispatch(killCmd);
-                LOGGER.traceMarker("checkTimeoutTask", "Send KILL for timeout cmd %s", cmd);
+            if (cmd.getType() != CmdType.RUN_SHELL || !cmd.isCurrent()) {
+                continue;
+            }
 
-                // update cmd status via queue
-                CmdStatusItem statusItem = new CmdStatusItem(cmd.getId(), CmdStatus.TIMEOUT_KILL, null, true, true);
-                cmdService.updateStatus(statusItem, true);
+            if (DateUtil.isTimeOut(cmd.getCreatedDate(), ZonedDateTime.now(), cmd.getTimeout())) {
+                try {
+                    Cmd killCmd = cmdService.create(new CmdInfo(cmd.getAgentPath(), CmdType.KILL, null));
+                    dispatch(killCmd);
+                    LOGGER.traceMarker("checkTimeoutTask", "Send KILL for timeout cmd %s", cmd);
+
+                    // update cmd status via queue
+                    CmdStatusItem statusItem = new CmdStatusItem(cmd.getId(), CmdStatus.TIMEOUT_KILL, null, true, true);
+                    cmdService.updateStatus(statusItem, true);
+                } catch (Throwable e) {
+                    LOGGER.warn(e.getMessage());
+                }
             }
         }
 
