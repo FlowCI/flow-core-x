e87a970ffedef281122cc9a1b21c0ccb22515f40
Yang Guo <gy@fir.im>
2017-12-28 11:50:15 +0800

    adapt cygwin bash

--- platform-cmd-runner/src/main/java/com/flow/platform/cmd/CmdExecutor.java
+++ platform-cmd-runner/src/main/java/com/flow/platform/cmd/CmdExecutor.java
@@ -18,10 +18,13 @@ package com.flow.platform.cmd;
 
 import com.flow.platform.domain.Cmd;
 import com.flow.platform.domain.CmdResult;
+import com.flow.platform.util.CommandUtil;
 import com.flow.platform.util.CommandUtil.Unix;
+import com.flow.platform.util.CommandUtil.Windows;
 import com.flow.platform.util.DateUtil;
 import com.flow.platform.util.Logger;
 import com.flow.platform.util.SystemUtil;
+import com.google.common.base.Strings;
 import com.google.common.collect.Sets;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -34,6 +37,7 @@ import java.io.OutputStreamWriter;
 import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -109,6 +113,8 @@ public final class CmdExecutor {
 
     private final static int DEFAULT_SHUTDOWN_WAITING_SECONDS = 30;
 
+    private final static String CYGWIN_HOME = "CYGWIN_HOME";
+
     private final ConcurrentLinkedQueue<Log> loggingQueue = new ConcurrentLinkedQueue<>();
 
     private final String endTerm = String.format("=====EOF-%s=====", UUID.randomUUID());
@@ -174,7 +180,7 @@ public final class CmdExecutor {
         cmds.add(0, "set -e"); // exit bash when command error
 
         this.cmdList = cmds;
-        this.pBuilder = new ProcessBuilder("/bin/bash").directory(DEFAULT_WORKING_DIR);
+        this.pBuilder = new ProcessBuilder(getExecutor()).directory(DEFAULT_WORKING_DIR);
 
         // check and init working dir
         if (workingDir != null) {
@@ -274,6 +280,20 @@ public final class CmdExecutor {
         }
 
         return outputResult;
+    }
+
+    private String getExecutor() {
+        if (SystemUtil.isWindows()) {
+
+            String cygwinHome = System.getenv(CYGWIN_HOME);
+            if (Strings.isNullOrEmpty(cygwinHome)) {
+                return Windows.CMD_EXECTOR;
+            }
+
+            return Paths.get(cygwinHome, "bin", "bash.exe").toString();
+        }
+
+        return Unix.CMD_EXECTOR;
     }
 
     /**
