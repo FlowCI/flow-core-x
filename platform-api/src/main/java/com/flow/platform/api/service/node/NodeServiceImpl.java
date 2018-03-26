e5a621826cffcb795c6f7269d1c576035ad71b30
will <yh@fir.im>
2018-03-26 17:04:06 +0800

    delete error state change

--- platform-api/src/main/java/com/flow/platform/api/service/node/NodeServiceImpl.java
+++ platform-api/src/main/java/com/flow/platform/api/service/node/NodeServiceImpl.java
@@ -126,7 +126,6 @@ public class NodeServiceImpl extends CurrentUser implements NodeService {
         try {
             rootFromYml = ymlService.build(flow, yml);
         } catch (YmlException e) {
-            updateYmlState(flow, FlowEnvs.YmlStatusValue.ERROR, e.getMessage());
             throw e;
         }
 
