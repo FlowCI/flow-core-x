93c5db54d0c900813a7eb106f90527a5a5f0db60
gy2006 <32008001@qq.com>
2020-10-26 11:31:38 +0100

    add ut for conditon test

--- core/src/main/java/com/flowci/core/flow/service/FlowSettingServiceImpl.java
+++ core/src/main/java/com/flowci/core/flow/service/FlowSettingServiceImpl.java
@@ -16,36 +16,20 @@
 
 package com.flowci.core.flow.service;
 
-import com.flowci.core.common.manager.ConditionManager;
-import com.flowci.core.common.manager.SpringEventManager;
 import com.flowci.core.common.manager.VarManager;
 import com.flowci.core.flow.dao.FlowDao;
-import com.flowci.core.flow.dao.YmlDao;
 import com.flowci.core.flow.domain.Flow;
 import com.flowci.core.flow.domain.Settings;
 import com.flowci.core.flow.domain.WebhookStatus;
-import com.flowci.core.flow.domain.Yml;
-import com.flowci.core.job.domain.Job;
-import com.flowci.core.job.event.CreateNewJobEvent;
-import com.flowci.core.trigger.domain.GitPingTrigger;
-import com.flowci.core.trigger.domain.GitTrigger;
-import com.flowci.core.trigger.event.GitHookEvent;
-import com.flowci.domain.StringVars;
 import com.flowci.domain.VarValue;
-import com.flowci.domain.Vars;
 import com.flowci.exception.ArgumentException;
-import com.flowci.tree.FlowNode;
-import com.flowci.tree.YmlParser;
 import com.flowci.util.StringHelper;
-import groovy.util.ScriptException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * @author yang
@@ -59,16 +43,7 @@ public class FlowSettingServiceImpl implements FlowSettingService {
     private FlowDao flowDao;
 
     @Autowired
-    private YmlDao ymlDao;
-
-    @Autowired
-    private SpringEventManager eventManager;
-
-    @Autowired
     private VarManager varManager;
-
-    @Autowired
-    private ConditionManager conditionManager;
 
     @Autowired
     private CronService cronService;
@@ -129,58 +104,5 @@ public class FlowSettingServiceImpl implements FlowSettingService {
         }
 
         flowDao.save(flow);
-    }
-
-    @EventListener
-    public void onGitHookEvent(GitHookEvent event) {
-        GitTrigger trigger = event.getTrigger();
-        Flow flow = flowDao.findByName(event.getFlow());
-
-        if (event.isPingEvent()) {
-            GitPingTrigger ping = (GitPingTrigger) trigger;
-
-            WebhookStatus ws = new WebhookStatus();
-            ws.setAdded(true);
-            ws.setCreatedAt(ping.getCreatedAt());
-            ws.setEvents(ping.getEvents());
-
-            flow.setWebhookStatus(ws);
-            flowDao.save(flow);
-            return;
-        }
-
-        if (trigger.isSkip()) {
-            log.info("Ignore git trigger {} since skip message", trigger);
-            return;
-        }
-
-        Optional<Yml> optional = ymlDao.findById(flow.getId());
-        if (!optional.isPresent()) {
-            log.warn("No available yml for flow {}", flow.getName());
-            return;
-        }
-
-        Yml yml = optional.get();
-        FlowNode root = YmlParser.load(flow.getName(), yml.getRaw());
-        if (!canStartJob(root, trigger)) {
-            log.debug("Cannot start job since filter not matched on flow {}", flow.getName());
-            return;
-        }
-
-        StringVars gitInput = trigger.toVariableMap();
-        Job.Trigger jobTrigger = trigger.toJobTrigger();
-
-        eventManager.publish(new CreateNewJobEvent(this, flow, yml.getRaw(), jobTrigger, gitInput));
-    }
-
-    private boolean canStartJob(FlowNode root, GitTrigger trigger) {
-        try {
-            String groovy = root.getCondition();
-            Vars<String> envs = trigger.toVariableMap();
-            return conditionManager.run(groovy, envs);
-        } catch (ScriptException e) {
-            log.warn("Illegal groovy script at condition section", e);
-            return false;
-        }
     }
 }
