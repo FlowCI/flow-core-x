e5c899fc8fb914322ef2a3045e8a68a7abc22aa0
gy2006 <32008001@qq.com>
2020-11-17 22:06:55 +0100

    change cache to step level only

--- tree/src/main/java/com/flowci/tree/Cache.java
+++ tree/src/main/java/com/flowci/tree/Cache.java
@@ -2,14 +2,12 @@ package com.flowci.tree;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.util.List;
 
-@Getter
 @Setter
-@Accessors(chain = true)
+@Getter
 public class Cache implements Serializable {
 
     private String key;
