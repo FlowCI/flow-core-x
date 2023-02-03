8b60b281ab4d6655f136fa984a7dca3ee89327a7
gy2006 <32008001@qq.com>
2023-02-03 22:28:43 +0100

    add cors filter

---
+++ core/src/main/java/com/flowci/core/common/adviser/CorsFilter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018 flow.ci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.flowci.core.common.adviser;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+
+/**
+ * @author yang
+ */
+@WebFilter(urlPatterns = "/*", filterName = "corsFilter")
+public class CorsFilter implements Filter {
+
+    private static final String AllowedHeaders =
+        "Origin, X-Requested-With, Content-Disposition, Content-Type, Accept, Token, Authorization";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (!isHttpRequest(request, response)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        var httpRequest = (HttpServletRequest) request;
+        var httpResponse = (HttpServletResponse) response;
+
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, DELETE");
+        httpResponse.setHeader("Access-Control-Max-Age", "1800");
+        httpResponse.setHeader("Access-Control-Allow-Headers", AllowedHeaders);
+        httpResponse.setHeader("Access-Control-Expose-Headers", AllowedHeaders);
+
+        if (httpRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private static boolean isHttpRequest(ServletRequest request, ServletResponse response) {
+        return (request instanceof HttpServletRequest) && (response instanceof HttpServletResponse);
+    }
+}
