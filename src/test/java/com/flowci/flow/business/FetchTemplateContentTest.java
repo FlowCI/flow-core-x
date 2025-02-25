f6dddadb349f0f85438a3a77fd7fcc6c617f6bab
gy2006 <32008001@qq.com>
2025-02-25 23:31:53 +0100

    get rid of pact, using wiremock instead

--- src/test/java/com/flowci/flow/business/FetchTemplateContentTest.java
+++ src/test/java/com/flowci/flow/business/FetchTemplateContentTest.java
@@ -1,31 +1,33 @@
 package com.flowci.flow.business;
 
-import au.com.dius.pact.consumer.MockServer;
-import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
-import au.com.dius.pact.consumer.junit5.PactConsumerTest;
-import au.com.dius.pact.consumer.junit5.PactIgnore;
-import au.com.dius.pact.consumer.junit5.PactTestFor;
-import au.com.dius.pact.core.model.V4Pact;
-import au.com.dius.pact.core.model.annotations.Pact;
 import com.flowci.SpringTest;
 import com.flowci.common.exception.NotAvailableException;
 import com.flowci.flow.model.YamlTemplate;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cache.CacheManager;
-import org.springframework.http.HttpHeaders;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-@PactConsumerTest
 class FetchTemplateContentTest extends SpringTest {
+
+    @RegisterExtension
+    private final static WireMockExtension WireMockServer =
+            WireMockExtension.newInstance()
+                    .options(wireMockConfig().dynamicPort())
+                    .build();
 
     @MockBean
     private FetchTemplates fetchTemplates;
@@ -36,24 +38,13 @@ class FetchTemplateContentTest extends SpringTest {
     @Autowired
     private CacheManager yamlTemplateCacheManager;
 
-    @Pact(provider = "TemplateContentProvider", consumer = "consumer_template_content")
-    V4Pact createPact(PactDslWithProvider builder) throws IOException {
-        return builder
-                .given("maven template")
-                .uponReceiving("")
-                .path("/git/templates/helloworld.yaml")
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .headers(Map.of(HttpHeaders.CONTENT_TYPE, "application/yaml"))
-                .body(getResourceAsString("template_helloworld.yaml"))
-                .toPact(V4Pact.class);
-    }
-
     @Test
-    @PactTestFor(providerName = "TemplateContentProvider")
-    void whenFetchingContentSuccessfully_thenReturnYamlContentFromPact(MockServer mockServer) {
-        var mockUrl = mockServer.getUrl() + "/git/templates/helloworld.yaml";
+    void whenFetchingContentSuccessfully_thenReturnYamlContent() {
+        var mockUrl = WireMockServer.url("/git/templates/helloworld.yaml");
+        WireMockServer.stubFor(
+                get("/git/templates/helloworld.yaml")
+                        .willReturn(ok(getResourceAsString("template_helloworld.yaml"))));
+
         when(fetchTemplates.invoke())
                 .thenReturn(List.of(
                         new YamlTemplate(
@@ -73,7 +64,6 @@ class FetchTemplateContentTest extends SpringTest {
     }
 
     @Test
-    @PactIgnore
     void whenFetchingContentNotFound_thenThrowException() {
         when(fetchTemplates.invoke()).thenReturn(Collections.emptyList());
         assertThrows(NotAvailableException.class, () -> fetchTemplateContent.invoke("helloworld"));
--- src/test/java/com/flowci/flow/business/FetchTemplatesTest.java
+++ src/test/java/com/flowci/flow/business/FetchTemplatesTest.java
@@ -1,26 +1,26 @@
 package com.flowci.flow.business;
 
-import au.com.dius.pact.consumer.MockServer;
-import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
-import au.com.dius.pact.consumer.junit5.PactConsumerTest;
-import au.com.dius.pact.consumer.junit5.PactTestFor;
-import au.com.dius.pact.core.model.V4Pact;
-import au.com.dius.pact.core.model.annotations.Pact;
 import com.flowci.SpringTest;
 import com.flowci.common.config.AppProperties;
 import com.flowci.common.exception.NotAvailableException;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
-import org.springframework.http.HttpHeaders;
 
-import java.io.IOException;
-import java.util.Map;
-
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.jupiter.api.Assertions.*;
 
-@PactConsumerTest
 class FetchTemplatesTest extends SpringTest {
+
+    @RegisterExtension
+    private final static WireMockExtension WireMockServer =
+            WireMockExtension.newInstance()
+                    .options(wireMockConfig().dynamicPort())
+                    .build();
 
     @Autowired
     private AppProperties appProperties;
@@ -31,36 +31,16 @@ class FetchTemplatesTest extends SpringTest {
     @Autowired
     private CacheManager yamlTemplateCacheManager;
 
-    @Pact(provider = "YamlTemplateProvider", consumer = "consumer_yaml_template")
-    V4Pact createPact(PactDslWithProvider builder) throws IOException {
-        return builder
-                .given("yaml templates are fetched")
-                .uponReceiving("")
-                .path("/git/templates.json")
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .headers(Map.of(HttpHeaders.CONTENT_TYPE, "application/json"))
-                .body(getResourceAsString("flow_templates.json"))
-                .toPact(V4Pact.class);
-    }
-
-    @Pact(provider = "YamlTemplateProviderWith5xx", consumer = "consumer_yaml_template")
-    V4Pact createPactWith5xx(PactDslWithProvider builder) throws IOException {
-        return builder
-                .given("yaml templates are fetched")
-                .uponReceiving("")
-                .path("/git/templates.json")
-                .method("GET")
-                .willRespondWith()
-                .status(500)
-                .toPact(V4Pact.class);
-    }
-
     @Test
-    @PactTestFor(providerName = "YamlTemplateProvider")
-    void whenFetchingSuccessfully_thenReturnListOfTemplates(MockServer mockServer) {
-        appProperties.getTemplates().setUrl(mockServer.getUrl() + "/git/templates.json");
+    void whenFetchingSuccessfully_thenReturnListOfTemplates() {
+        WireMockServer.stubFor(get("/git/templates.json")
+                .willReturn(aResponse()
+                        .withBody(getResourceAsString("flow_templates.json"))
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")));
+
+        appProperties.getTemplates()
+                .setUrl(WireMockServer.url("/git/templates.json"));
 
         var templates = fetchTemplates.invoke();
         assertNotNull(templates);
@@ -68,9 +48,13 @@ class FetchTemplatesTest extends SpringTest {
     }
 
     @Test
-    @PactTestFor(providerName = "YamlTemplateProviderWith5xx")
-    void whenFetchingWith5xx_thenReturnListOfTemplates(MockServer mockServer) {
-        appProperties.getTemplates().setUrl(mockServer.getUrl() + "/git/templates.json");
+    void whenFetchingWith5xx_thenThrowNotAvailableException() {
+        WireMockServer.stubFor(get("/git/templates.json")
+                .willReturn(aResponse().withStatus(500)));
+
+        appProperties.getTemplates()
+                .setUrl(WireMockServer.url("/git/templates.json"));
+
         assertThrows(NotAvailableException.class, () -> fetchTemplates.invoke());
     }
 }
