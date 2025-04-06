b29532a8dc65411f3b82a1ddf1d9b3b51258849a
gy2006 <32008001@qq.com>
2025-04-06 20:53:45 +0200

    update job status to queued on wait for agent

--- src/main/java/com/flowci/build/business/impl/WaitForAgentImpl.java
+++ src/main/java/com/flowci/build/business/impl/WaitForAgentImpl.java
@@ -2,18 +2,24 @@ package com.flowci.build.business.impl;
 
 import com.flowci.build.business.WaitForAgent;
 import com.flowci.build.model.Build;
+import com.flowci.build.model.Job;
 import com.flowci.build.repo.BuildRepo;
+import com.flowci.build.repo.JobRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @AllArgsConstructor
 public class WaitForAgentImpl implements WaitForAgent {
 
     private final BuildRepo buildRepo;
+    private final JobRepo jobRepo;
 
     @Override
+    @Transactional
     public void invoke(Long buildId) {
         buildRepo.updateBuildStatusById(buildId, Build.Status.QUEUED);
+        jobRepo.updateJobStatusByBuildId(buildId, Job.Status.QUEUED);
     }
 }
--- src/main/java/com/flowci/build/repo/JobRepo.java
+++ src/main/java/com/flowci/build/repo/JobRepo.java
@@ -2,8 +2,16 @@ package com.flowci.build.repo;
 
 import com.flowci.build.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface JobRepo extends JpaRepository<Job, Job.Id> {
+
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update Job j set j.status = ?2 where j.id.buildId= ?1")
+    void updateJobStatusByBuildId(Long buildId, Job.Status status);
 }
